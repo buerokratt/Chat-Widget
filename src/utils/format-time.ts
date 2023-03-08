bfae829ad35a8e5c463c70deb4bbf7495bd806a5
Sergei Rudz <sergei.rudz@we.ee>
2023-03-08 11:50:13 +0200

    fix format time util

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -1,9 +1,6 @@
-import { useTranslation } from 'react-i18next';
 const formatTime = (time: number) => {
-  const t = useTranslation;
   const minutes = Math.floor(time / 60);
-  if (minutes > 0) return `${minutes} ${t('widget.time.minutes')}`;
-  else if (minutes === 1) return `${minutes} ${t('widget.time.minute')}`;
+  return minutes;
 };
 
 export default formatTime;
