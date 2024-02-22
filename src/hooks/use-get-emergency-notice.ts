d906223cbf78ab2f9ad1eb918fbab715ab46985e
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-02-23 01:56:47 +0200

    Removed Console.log

--- src/hooks/use-get-emergency-notice.ts
+++ src/hooks/use-get-emergency-notice.ts
@@ -15,11 +15,8 @@ const useGetEmergencyNotice = (): void => {
     }
 
     if (!messages.map((m) => m.event).includes(CHAT_EVENTS.EMERGENCY_NOTICE)) {
-      console.log("hi");
       if (!emergencyNotice.isVisible) return;
-      console.log("hi2");
       if (new Date(emergencyNotice.start) <= new Date() && new Date(emergencyNotice.end) >= new Date()) {
-        console.log("hi3");
         dispatch(
           addMessage({
             chatId,
