d4d18652ddc50e92ee6d0e4549fc5296be6ca2f3
baha-a <bhlshrf@gmail.com>
2024-03-27 17:57:52 +0100

    Stop chat termination in case of reloading last browser tab

--- src/hooks/use-reload-chat-end-effect.ts
+++ src/hooks/use-reload-chat-end-effect.ts
@@ -1,21 +1,43 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "../store";
 import { CHAT_EVENTS } from "../constants";
-import { endChat } from "../slices/chat-slice";
+import { addChatToTerminationQueue, endChat, removeChatFromTerminationQueue } from "../slices/chat-slice";
 import useChatSelector from "./use-chat-selector";
 import { isRedirectPathEmpty } from "../utils/auth-utils";
+
+const isLastSession = () => {
+  const sessions = localStorage.getItem("sessions");
+  return sessions && parseInt(sessions) === 1;
+}
+
+const wasPageReloaded = () => {
+  return window.performance
+    .getEntriesByType('navigation')
+    .map((nav) => (nav as PerformanceNavigationTiming).type)
+    .includes('reload');
+}
+
+const isChatAboutToBeTerminated = () => {
+  const terminationTime = sessionStorage.getItem('terminationTime');
+  if(terminationTime)
+    return 2000 < (Date.now() - parseInt(terminationTime));
+  return false;
+}
 
 const useReloadChatEndEffect = () => {
   const { chatId } = useChatSelector();
   const dispatch = useAppDispatch();
 
+  if(wasPageReloaded() && isChatAboutToBeTerminated()) {
+    dispatch(removeChatFromTerminationQueue())
+  }
+
   useEffect(() => {
-    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      const sessions = localStorage.getItem("sessions");
-      if (chatId && isRedirectPathEmpty() && sessions && parseInt(sessions) === 1) {
+    const handleBeforeUnload = () => {
+      if (chatId && isRedirectPathEmpty() && isLastSession()) {
         localStorage.setItem("sessions", "1");
         dispatch(
-          endChat({
+          addChatToTerminationQueue({
             event: CHAT_EVENTS.CLIENT_LEFT_FOR_UNKNOWN_REASONS,
             isUpperCase: true,
           })
@@ -25,7 +47,7 @@ const useReloadChatEndEffect = () => {
 
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "F5" || (event.ctrlKey && event.key === "r")) {
-        handleBeforeUnload(event);
+        handleBeforeUnload();
       }
     };
 
