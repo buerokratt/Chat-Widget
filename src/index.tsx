bb0cc3dd5f532147e343fe698b5a7d451194ec2a
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-21 13:09:07 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
@@ -10,7 +10,13 @@ import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to="/byk-widget" />} />
+        <Route path="/byk-widget" Component={App} />
+        <Route path="/auth/callback" Component={AuthCallback} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('byk-va'),
 );
