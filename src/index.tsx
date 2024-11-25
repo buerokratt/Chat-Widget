15db9b354d22d768ddaf3a86659c2e411093b72c
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-25 15:44:27 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,13 +10,11 @@ import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
       <Routes>
         <Route index element={<Navigate to="/byk-widget" />} />
         <Route path="/byk-widget" element={<App />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
       </Routes>
-    </BrowserRouter>
   </Provider>,
   document.getElementById("byk-va")
 );
