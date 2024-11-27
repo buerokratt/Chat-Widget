481e72df627916339c46d33bdd642b3c51cef7ea
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-27 15:27:08 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './store';
-import AuthCallback from './AuthCallback';
-import './i18n';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import AuthCallback from "./AuthCallback";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+// Automatically detect the subpath from the current location
+const getBasePath = () => {
+  const { pathname } = window.location;
+  // Assume the app is served from the first segment of the path
+  const basePath = pathname.split("/")[1];
+  return `/${basePath}`;
+};
+
+const baseName = getBasePath();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={baseName}>
       <Routes>
-        {/* Dynamically match any subpath */}
-        <Route path="/*" element={<App />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/" Component={App} />
+        <Route path="/auth/callback" Component={AuthCallback} />
       </Routes>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('byk-va'),
+  document.getElementById("byk-va"),
 );
