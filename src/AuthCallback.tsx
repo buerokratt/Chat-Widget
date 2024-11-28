6bbefec84216dc636e8cb22866d287d694ac3016
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-28 15:00:22 +0200

    Update AuthCallback.tsx

--- src/AuthCallback.tsx
+++ src/AuthCallback.tsx
@@ -1,23 +1,15 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux'; // Import useDispatch to dispatch actions
-import { setIsAuthenticated } from './slices/authentication-slice'; // Import the action
 
 const AuthCallback = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch(); // Get the dispatch function
 
   useEffect(() => {
-    const handleAuthentication = async () => {
-      dispatch(setIsAuthenticated());
-      navigate('/', { replace: true });
-    };
-
-    handleAuthentication();
-  }, [dispatch, navigate]);
+    navigate('/', { replace: true });
+  }, []);
 
   return (
-    <span>Processing authentication...</span>
+    <span>processing authentication...</span>
   );
 };
 
