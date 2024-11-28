8f4dea7c010fc1f7606ce5ae3be8855561eb50b6
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-28 14:15:02 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
 import AuthCallback from './AuthCallback';
 import './i18n';
 import './index.scss';
+
 
 const getBasePath = () => {
   const { pathname } = window.location;
@@ -16,30 +17,14 @@ const getBasePath = () => {
 
 const baseName = getBasePath();
 
-const Root = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname === '/auth/callback') {
-      setTimeout(() => {
-        window.location.reload();
-      }, 100); 
-    }
-  }, [location]);
-
-  return (
-    <Routes>
-      <Route path="/" Component={App} />
-      <Route path="/auth/callback" Component={AuthCallback} />
-    </Routes>
-  );
-};
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={baseName}>
-      <Root />
+      <Routes>
+        <Route path="/" Component={App} />
+        <Route path="/auth/callback" Component={AuthCallback} />
+      </Routes>
     </BrowserRouter>
   </Provider>,
-  document.getElementById("byk-va")
+  document.getElementById("byk-va"),
 );
