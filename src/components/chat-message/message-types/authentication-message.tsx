d9924381d0b6ca4fc336ddabeb742f42858b7d47
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-28 16:42:50 +0200

    Update authentication-message.tsx

--- src/components/chat-message/message-types/authentication-message.tsx
+++ src/components/chat-message/message-types/authentication-message.tsx
@@ -4,19 +4,11 @@ import useChatSelector from "../../../hooks/use-chat-selector";
 import useAuthenticationSelector from "../../../hooks/use-authentication-selector";
 import styles from "../chat-message.module.scss";
 import { redirectToTim } from "../../../utils/auth-utils";
-import { useNavigate } from "react-router-dom";
 
 const AuthenticationMessage = (): JSX.Element => {
   const { t } = useTranslation();
   const { chatId, isChatEnded } = useChatSelector();
   const { isAuthenticated } = useAuthenticationSelector();
-  const navigate = useNavigate(); 
-
-  const handleAuthentication = () => {
-    const currentPath = window.location.pathname;
-    
-    navigate("/auth", { state: { from: currentPath } });
-  };
 
   return (
     <>
@@ -25,7 +17,7 @@ const AuthenticationMessage = (): JSX.Element => {
       </div>
       <button
         disabled={isChatEnded || isAuthenticated}
-        onClick={handleAuthentication} // Call the new handler
+        onClick={redirectToTim}
         className={`${styles["event-button"]} ${
           isAuthenticated ? styles.authenticated : ""
         }`}
@@ -41,4 +33,3 @@ const AuthenticationMessage = (): JSX.Element => {
 };
 
 export default AuthenticationMessage;
-
