de9b93d1d5a0119d8617293e65bc6dbd0c0db12f
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-28 14:04:13 +0200

    Update AuthCallback.tsx

--- src/AuthCallback.tsx
+++ src/AuthCallback.tsx
@@ -5,12 +5,8 @@ const AuthCallback = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    
     navigate('/', { replace: true });
-    setTimeout(() => {
-      window.location.reload();
-    }, 100); 
-  }, [navigate]);
+  }, []);
 
   return (
     <span>processing authentication...</span>
