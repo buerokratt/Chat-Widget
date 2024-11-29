00e41a7c29a85110db716cbbec6e1d2cf9f76d88
Varmo <101868197+varmoh@users.noreply.github.com>
2024-11-29 10:49:24 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={baseName}>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/${basePath}" element={<App />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
       </Routes>
     </BrowserRouter>
