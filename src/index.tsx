e3f2119499a775e512ec58ff4f08fb059f464dd3
Varmo <101868197+varmoh@users.noreply.github.com>
2024-12-16 14:41:49 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
@@ -8,22 +8,14 @@ import AuthCallback from './AuthCallback';
 import './i18n';
 import './index.scss';
 
-const getBasePath = () => {
-  const { pathname } = window.location;
-  const basePath = pathname.split("/")[1];
-  console.log(basePath);
-  return `/${basePath}`;
-};
-
-const baseName = getBasePath();
-
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={baseName}>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById("byk-va")
 );
