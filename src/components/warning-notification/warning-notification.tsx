27538c3d638ea5846a4b4fc74ee625003e89f8e2
VassiliMoskaljov <vassilimoskaljov@gmail.com>
2025-03-05 17:43:52 +0200

    feat(384): Added global byk-chat styling

--- src/components/warning-notification/warning-notification.tsx
+++ src/components/warning-notification/warning-notification.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface WarningNotificationProps {
-  warningMessage: string;
+    warningMessage: string;
 }
 
 const WarningNotification = (props: WarningNotificationProps): JSX.Element => {
-  const { warningMessage = '' } = props;
-  return (
-    <WarningStyle>
-      <div className="warning">{warningMessage}</div>
-    </WarningStyle>
-  );
+    const {warningMessage = ''} = props;
+    return (
+        <div className="byk-chat">
+            <WarningStyle>
+                <div className="warning">{warningMessage}</div>
+            </WarningStyle>
+        </div>
+    );
 };
 
 const WarningStyle = styled.div`
-  .warning {
-    text-align: center;
-    padding: 0.25em;
-    color: #3c0078;
-    background-color: #fcedc5;
-  }
+    :global(.byk-chat) {
+        .warning {
+            text-align: center;
+            padding: 0.25em;
+            color: #3c0078;
+            background-color: #fcedc5;
+        }
+    }
 `;
 export default WarningNotification;
