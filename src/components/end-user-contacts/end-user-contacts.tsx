76ac6e3d30f0ba3e2d27c75a628fbb9a0b03b286
VassiliMoskaljov <vassilimoskaljov@gmail.com>
2025-03-06 15:26:39 +0200

    feat(384): End user contact data.

--- src/components/end-user-contacts/end-user-contacts.tsx
+++ src/components/end-user-contacts/end-user-contacts.tsx
@@ -74,7 +74,7 @@ const EndUserContacts = (): JSX.Element => {
     };
 
     return (
-        <div className="byk-chat">
+        <EndUserContactsStyle>
             <form>
                 <div className="container">
                     <h3 className="form-header">{t("widget.contacts.contact.header")}</h3>
@@ -131,7 +131,7 @@ const EndUserContacts = (): JSX.Element => {
                     </div>
                 </div>
             </form>
-        </div>
+        </EndUserContactsStyle>
     );
 };
 
@@ -140,7 +140,6 @@ const TextAreaStyle = styled.textarea`
     outline: 0;
     border: 0;
     background: transparent;
-    resize: vertical;
     overflow: auto;
     resize: none;
 `;
@@ -148,76 +147,75 @@ const TextAreaStyle = styled.textarea`
 const EndUserContactsStyle = styled.div`
     height: 100%;
 
-    :global(.byk-chat) {
-        input,
-        textarea {
-            border: 0;
-            border-bottom: 1px solid #003cff;
-            padding-bottom: 5px;
+    input,
+    textarea {
+        border: 0;
+        border-bottom: 1px solid #003cff;
+        padding-bottom: 5px;
+        width: 100%;
+    }
+
+    h5 {
+        margin: 0.5rem 0 0 0;
+    }
+
+    .form-header {
+        margin-bottom: 1rem;
+    }
+
+    .form-footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1rem;
+        gap: 0.5rem;
+    }
+
+    form {
+        height: 115%;
+        width: 100%;
+        padding: 0.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin-top: -3rem;
+    }
+
+    .form-body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: medium;
+
+        .email-group,
+        .phone-nr-group {
+            padding-bottom: 1rem;
             width: 100%;
         }
 
-        h5 {
-            margin: 0.5rem 0 0 0;
-        }
-
-        .form-header {
-            margin-bottom: 1rem;
-        }
-
-        .form-footer {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 1rem;
-            gap: 0.5rem;
-        }
-
-        form {
-            height: 115%;
+        .comment-group {
             width: 100%;
-            padding: 0.5rem;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            margin-top: -3rem;
         }
 
-        .form-body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            font-size: medium;
-
-            .email-group,
-            .phone-nr-group {
-                padding-bottom: 1rem;
-                width: 100%;
-            }
-
-            .comment-group {
-                width: 100%;
-            }
-
-            .email-input:invalid,
-            .phone-nr-input:invalid {
-                border-color: #ff4800;
-            }
+        .email-input:invalid,
+        .phone-nr-input:invalid {
+            border-color: #ff4800;
         }
+    }
 
-        .input-labels {
-            display: flex;
-            align-items: flex-start;
-            justify-items: flex-start;
-        }
+    .input-labels {
+        display: flex;
+        align-items: flex-start;
+        justify-items: flex-start;
+    }
 
-        .missing-feeback {
-            color: #ff4800;
-            margin-bottom: -0.5em;
-            font-size: 0.75rem;
-        }
+    .missing-feeback {
+        color: #ff4800;
+        margin-bottom: -0.5em;
+        font-size: 0.75rem;
+
     }
 `;
 
