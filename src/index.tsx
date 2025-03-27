db98226f1730fc309a8c0f7cd4500d498ef336f0
Varmo <101868197+varmoh@users.noreply.github.com>
2025-03-27 11:03:23 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './store';
-import AuthCallback from './AuthCallback';
-import './i18n';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./store";
+import "./i18n";
+import "./index.scss";
 
 const getBasePath = () => {
   const { pathname } = window.location;
+  if (!pathname || pathname === "/") return "/";
   const basePath = pathname.split("/")[1];
-  console.log(basePath);
   return `/${basePath}`;
 };
 
@@ -21,9 +20,9 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={baseName}>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </Provider>,
-  document.getElementById("byk-va")
+  document.getElementById('byk-va'),
 );
