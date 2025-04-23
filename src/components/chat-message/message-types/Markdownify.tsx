19c36f1a19e7e8e01e6660022bd0717c7e1bbcc3
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-04-23 03:10:12 +0200

    chore(443): Modified Replace

--- src/components/chat-message/message-types/Markdownify.tsx
+++ src/components/chat-message/message-types/Markdownify.tsx
@@ -58,7 +58,7 @@ const Markdownify: React.FC<MarkdownifyProps> = ({ message, sanitizeLinks = fals
     >
       {message
         ?.replace(/&#x([0-9A-Fa-f]+);/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)))
-        ?.replace(/(?<=\n)\d+\.\s/g, /^.*\..*:\n\n/.test(message) ? "\n\n$&" : "") ?? ""}
+        ?.replace(/(?<=\n)\d+\.\s/g, /^.*\..*:\n\n/.test(message) ? "\n\n$&" : "$&") ?? ""}
     </Markdown>
   </div>
 );
