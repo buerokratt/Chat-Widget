5a23a50c26b147e63edad7b959fec0cc0f53abc5
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-01-06 23:44:02 +0200

    chore(458): Added announcement to event message

--- src/components/chat-message/message-types/event-message.tsx
+++ src/components/chat-message/message-types/event-message.tsx
@@ -1,12 +1,27 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useRef } from "react";
 import { motion } from "framer-motion";
 import { ChatMessageStyled } from "../ChatMessageStyled";
+import useScreenReaderAnnouncement from "../../../hooks/use-screen-reader-announcement";
 
 const EventMessage = (props: { content: ReactElement }): JSX.Element => {
   const { content } = props;
+  const screenReaderRef = useRef<HTMLDivElement>(null);
+  useScreenReaderAnnouncement(content.props.children.toString(), screenReaderRef);
 
   return (
     <motion.div>
+      <div
+        ref={screenReaderRef}
+        aria-live="polite"
+        aria-atomic="false"
+        style={{
+          position: "absolute",
+          left: "-10000px",
+          width: "0.1px",
+          height: "0.1px",
+          overflow: "hidden",
+        }}
+      />
       <ChatMessageStyled className={`message event`}>
         <div>
           <div aria-live="polite">{content}</div>
