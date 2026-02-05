65fb60e0f4e3207e27c79731b7c86a37010dd238
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-02-05 21:57:31 +0200

    chore(1764): Moved emergency notice ui under subtitle

--- src/hooks/use-get-emergency-notice.ts
+++ src/hooks/use-get-emergency-notice.ts
@@ -1,40 +1,17 @@
 import { useEffect } from "react";
-import { CHAT_EVENTS } from "../constants";
-import { getEmergencyNotice, addMessageToTop } from "../slices/chat-slice";
+import { getEmergencyNotice } from "../slices/chat-slice";
 import { useAppDispatch } from "../store";
 import useChatSelector from "./use-chat-selector";
-import { format } from "date-fns";
 
 const useGetEmergencyNotice = (): void => {
-  const { emergencyNotice, messages, chatId } = useChatSelector();
+  const { emergencyNotice } = useChatSelector();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (emergencyNotice === null) {
       dispatch(getEmergencyNotice());
-      return;
     }
-
-    if (!messages.map((m) => m.event).includes(CHAT_EVENTS.EMERGENCY_NOTICE)) {
-      if (!emergencyNotice.isVisible) return;
-      const formatDate = (date: Date) => new Date(format(date, "yyyy-MM-dd"));
-      const startDate = formatDate(new Date(emergencyNotice.start));
-      const endDate = formatDate(new Date(emergencyNotice.end));
-      const currentDate = formatDate(new Date());
-
-      if (startDate <= currentDate && endDate >= currentDate) {
-        dispatch(
-          addMessageToTop({
-            chatId,
-            event: CHAT_EVENTS.EMERGENCY_NOTICE,
-            content: emergencyNotice.text,
-            authorTimestamp: new Date().toISOString(),
-            authorRole: "chatbot",
-          })
-        );
-      }
-    }
-  }, [dispatch, emergencyNotice, chatId]);
+  }, [dispatch, emergencyNotice]);
 };
 
 export default useGetEmergencyNotice;
