57cab8ebbb006b8c60229eedea57a8a21f523329
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2026-09-10 11:39:51 +0300

    chore(753): Added widget interceptor

--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { RuuterResponse } from '../model/ruuter-response-model';
+import { SESSION_STORAGE_CHAT_ID_KEY } from '../constants';
+import { getFromLocalStorage } from '../utils/local-storage-utils';
+import { WIDGET_INSTANCE_ID } from '../utils/widget-instance-utils';
 
 const http = axios.create({
   baseURL: window._env_.RUUTER_API_URL,
@@ -8,6 +11,16 @@ const http = axios.create({
     'Cache-Control': 'no-cache, no-store, must-revalidate',
   },
   withCredentials: true,
+});
+
+http.interceptors.request.use((config: any) => {
+  if (WIDGET_INSTANCE_ID) {
+    const chatId = getFromLocalStorage(SESSION_STORAGE_CHAT_ID_KEY);
+    if (chatId) {
+      config.params = { ...config.params, chatId };
+    }
+  }
+  return config;
 });
 
 http.interceptors.response.use((response: any) => {
